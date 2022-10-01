77b9685d17f67ac7a2ff9b8d99ceb0952be57b33
lucaslsg-prog <lsg.comunicacoes@gmail.com>
2022-09-30 22:01:56 -0300

    inicio implementação classe sensores

--- Sensores.py
+++ Sensores.py
@@ -1,27 +1,5 @@
-from ComponentesEletromecanicos import Sensores
+class Temperatura:
+    pass
 
-class SensorTemperatura(Sensores):
-    def __init__(self):
-        super().__init__()
-
-    def set_grandeza(self, grandeza):
-        self.grandeza = grandeza
-    def get_grandeza(self):
-        return self.grandeza
-
-    def set_estado(self,estado):
-        self.estado = estado
-    def get_estado(self):
-        return self.estado
-
-    def ligar(self,estado):
-        self.set_estado('Sensor de temperatura ligado')
-    def desligar(self,estado):
-        self.set_estado('Sensor de temperatura desligado')
-
-    def medir(self):
-        self.get_grandeza
-        
-
-class SensorPressao(Sensores):
+class Pressao:
     pass
